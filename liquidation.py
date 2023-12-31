b7decf67e8b883f4f695205438b66d5519c0d218
hamzus95 <42734659+hamzus95@users.noreply.github.com>
2023-12-31 21:05:24 +0100

    securing credentials

--- liquidation.py
+++ liquidation.py
@@ -6,8 +6,9 @@ from datetime import datetime, timedelta
 from twilio.rest import Client
 
 # Twilio credentials
-account_sid = 'ACc3fdea95870c434343f572fb98d27fad'
-auth_token = '935c2b91b2e6c4b0e227b23da9c01097'
+
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 # Twilio WhatsApp number and recipient number
 whatsapp_number = 'whatsapp:+14155238886'  # Twilio's Sandbox number
